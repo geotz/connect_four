16953d696ac62651d7a90d075f74cc1c98b4c115
George <george@riven.local>
2014-08-06 01:29:38 +0300

    copyright notice

--- alphabeta.h
+++ alphabeta.h
@@ -1,5 +1,24 @@
+/*
+    Alpha-Beta Tree Search (c) 2014 George M. Tzoumas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _ALPHABETA_H_
 #define _ALPHABETA_H_
+
+// Alpha-Beta Tree Search
 
 #include<iostream>
 
